3ac18bc21202ce493d0a4863da525dc200c46dca
Purushottam <purushottamk@vmokshagroup.com>
2017-01-23 10:11:43 +0530

    Initial Commit

---
+++ FireBaseTesting/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FireBaseTesting
+//
+//  Created by vmoksha mobility on 1/23/17.
+//  Copyright © 2017 vmoksha mobility. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FireBaseTesting/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FireBaseTesting
+//
+//  Created by vmoksha mobility on 1/23/17.
+//  Copyright © 2017 vmoksha mobility. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
